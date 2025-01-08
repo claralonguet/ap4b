710254e782d1ccd4ff963c10b660126537909b66
claralonguet <151842131+claralonguet@users.noreply.github.com>
2025-01-08 15:04:00 +0100

    Update data.java

--- data.java
+++ data.java
@@ -4,7 +4,7 @@ import java.util.List;
 // Classe Data
 public class Data {
     private List<Integer> critere;
-    private List<List<Integer>> code;
+    private List<Integer> code;
 
     public Data() {
         this.critere = new ArrayList<>();
@@ -17,47 +17,46 @@ public class Data {
     }
 
     // Getter pour la liste 'code'
-    public List<List<Integer>> getCode() {
+    public List<Integer> getCode() {
         return code;
     }
 
-    // Setter pour les listes 'critere' et 'code'
-    public void setCritereAndCode(List<Integer> critere, List<List<Integer>> code) throws InvalidDataException {
-        if (critere == null || code == null) {
-            throw new InvalidDataException("Les listes de critère et de code ne peuvent pas être nulles");
-        }
-        if (critere.size() != code.size()) {
-            throw new InvalidDataException("Les listes de critère et de code doivent avoir la même taille");
+    // Setter pour la liste 'critere'
+    public void setCritere(List<Integer> critere) throws InvalidDataException {
+        if (critere == null) {
+            throw new InvalidDataException("La liste de critère ne peut pas être nulle");
         }
         this.critere = critere;
+    }
+
+    // Setter pour la liste 'code'
+    public void setCode(List<Integer> code) throws InvalidDataException {
+        if (code == null) {
+            throw new InvalidDataException("La liste de code ne peut pas être nulle");
+        }
         this.code = code;
     }
 
     // Méthode pour ajouter un critère
     public void addCritere(Integer critere) throws InvalidDataException {
-        if (critere == null) {
+        if (criterion == null) {
             throw new InvalidDataException("Le critère ne peut pas être nul");
         }
         this.critere.add(critere);
-        this.code.add(new ArrayList<>());  // Ajout d'une liste vide pour maintenir la correspondance des tailles
     }
 
     // Méthode pour ajouter un code
-    public void addCode(List<Integer> codeEntry) throws InvalidDataException {
-        if (codeEntry == null || codeEntry.isEmpty()) {
-            throw new InvalidDataException("La valeur du code ne peut pas être nulle ou vide");
+    public void addCode(Integer codeEntry) throws InvalidDataException {
+        if (codeEntry == null) {
+            throw new InvalidDataException("La valeur du code ne peut pas être nulle");
         }
-        if (this.critere.size() <= this.code.size()) {
-            throw new InvalidDataException("Aucun critère correspondant n'est disponible pour ce code");
-        }
-        this.code.set(this.code.size() - 1, codeEntry);  // Remplacer la liste vide par la liste de code
+        this.code.add(codeEntry);
     }
 
     // Méthode pour afficher les données
     public void displayData() {
-        for (int i = 0; i < critere.size(); i++) {
-            System.out.println("Critère : " + critere.get(i) + " - Réponse : " + code.get(i));
-        }
+        System.out.println("Critères : " + critere);
+        System.out.println("Codes : " + code);
     }
 }
 
